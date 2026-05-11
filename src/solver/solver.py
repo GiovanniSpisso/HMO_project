b92301165a3eb39f5d4a823a96f5a445f163a719
RiccardoZattra <riccardo.zattra19@gmail.com>
2026-05-11 19:11:21 +0200

    MOdified solver.py to call the greedy plus local search algorithm

--- src/solver/solver.py
+++ src/solver/solver.py
@@ -4,6 +4,7 @@ import time
 from src.io.parser import parse_instance
 from src.io.writer import write_solution_file, update_results_csv
 from src.heuristics.greedy import greedy
+from src.heuristics.construction import greedy_local_search
  
 def solve_instance(instance_path):
     """
@@ -13,21 +14,32 @@ def solve_instance(instance_path):
     - print feasible solution lines
     - save each incumbent to a .sol file
     """
-    start = time.time()
 
     m, n, costs, columns = parse_instance(instance_path)
     instance_name = os.path.splitext(os.path.basename(instance_path))[0]
 
-    best_objective = None
-    best_columns = None
+    #best_objective = None
+    #best_columns = None
     solution_count = 1
 
-    # Placeholder: no real heuristic yet, just a dummy solution that takes all columns
+    #----------------------PURE GREEDY IMPLEMENTATION-------------------------------
+    start = time.time()
     obj, selected = greedy(m, n, costs, columns)
     elapsed = time.time() - start
     
     write_solution_file(instance_path, solution_count, obj, selected, output_dir="solutions/greedy")
     update_results_csv("solutions/greedy/results.csv", instance_name, obj, elapsed)
     
-    print("Greedy algorithm:\n")
-    print(f"Feasible solution of value {obj} [time {elapsed:.3f}]")
+    print("Greedy algorithm:")
+    print(f"Feasible solution of value {obj} [time {elapsed:.3f}]\n")
+
+    #----------------------GREEDY PLUS LOCAL SEARCH-------------------------------------------------
+    start = time.time()
+    obj, selected = greedy_local_search(m, n, costs, columns)
+    elapsed = time.time() - start
+
+    write_solution_file(instance_path, solution_count, obj, selected, output_dir="solutions/greedy_plus_local_search")
+    update_results_csv("solutions/greedy_plus_local_search/results.csv", instance_name, obj, elapsed)
+
+    print("Greedy plus local search:")
+    print(f"Feasible solution of value {obj} [time {elapsed:.3f}]\n")
